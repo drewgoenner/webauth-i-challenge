a708dc3f5cb8ed5d043b6ad8cdd1c55a03ce5b71
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-10-15 15:22:02 -0500

    completed MVP

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session')
 
 const userRouter = require('./users/userRouter.js');
+const protected = require('./users/protected-middleware.js');
 
 const server = express();
 
+const sessionConfig = {
+    name: 'flippyflappy', 
+    secret: 'no one can know',
+    cookie: {
+        maxAge: 1000 * 30,
+        secure: false, //true in production
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false, // GDPR laws against setting cookies automatically
+}
+
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
 server.use('/api/', userRouter);
+server.use('/api/restricted', protected, userRouter);
 
 module.exports = server;
--- users/protected-middleware.js
+++ users/protected-middleware.js
@@ -1,25 +1,14 @@
-const bcrypt = require('bcryptjs');
-const Users = require('./user-model.js');
+
 
 module.exports = protected
 
 function protected(req, res, next) {
-    const { username, password } = req.headers;
 
-    if (username && password) {
-        Users.findBy({ username })
-          .first()
-          .then(user => {
-              if (user && bcrypt.compareSync(password, user.password)){
-                  next();
-              } else {
-                  res.status(401).json({ message: 'invalid credentials submitted' })
-              }
-          })
-          .catch(err => {
-              res.status(500).json({ message: 'unexpected error'})
-          })
+    if (req.session && req.session.user) {
+        
+        next();
+            
     } else {
-        res.status(400).json({ message: 'no credentials submitted' })
+        res.status(401).json({ message: 'not authorized' })
     }
 }
